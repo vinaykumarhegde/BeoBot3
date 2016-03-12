3f1a41e6310e47f7b6f572c1bee897dc8440191f
Vinaykumar Hegde <me@vnay.in>
2016-03-11 18:55:11 -0800

    add support for ROSArduinoBridge

---
+++ motor_driver.ino
@@ -0,0 +1,89 @@
+/***************************************************************
+   Motor driver definitions
+   
+   Add a "#elif defined" block to this file to include support
+   for a particular motor driver.  Then add the appropriate
+   #define near the top of the main ROSArduinoBridge.ino file.
+   
+   *************************************************************/
+#include "motor_driver.h"
+
+#ifdef USE_BASE
+   
+#if defined POLOLU_VNH5019
+  /* Include the Pololu library */
+  #include "DualVNH5019MotorShield.h"
+
+  /* Create the motor driver object */
+  DualVNH5019MotorShield drive;
+  
+  /* Wrap the motor driver initialization */
+  void initMotorController() {
+    drive.init();
+  }
+
+  /* Wrap the drive motor set speed function */
+  void setMotorSpeed(int i, int spd) {
+    if (i == LEFT) drive.setM1Speed(spd);
+    else drive.setM2Speed(spd);
+  }
+
+  // A convenience function for setting both motor speeds
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);
+  }
+#elif defined POLOLU_MC33926
+  /* Include the Pololu library */
+  #include "DualMC33926MotorShield.h"
+
+  /* Create the motor driver object */
+  DualMC33926MotorShield drive;
+  
+  /* Wrap the motor driver initialization */
+  void initMotorController() {
+    drive.init();
+  }
+
+  /* Wrap the drive motor set speed function */
+  void setMotorSpeed(int i, int spd) {
+    if (i == LEFT) drive.setM1Speed(spd);
+    else drive.setM2Speed(spd);
+  }
+
+  // A convenience function for setting both motor speeds
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    setMotorSpeed(LEFT, leftSpeed);
+    setMotorSpeed(RIGHT, rightSpeed);
+  }
+
+#elif defined DFRHCR_N1_DRIVER
+
+  void initMotorController(){
+    
+  }
+  
+  /* Wrap the drive motor set speed function */
+  void setMotorSpeed(int i, int spd) {
+    // Empty function. This is not supported by motor driver.
+  }
+
+  // A convenience function for setting both motor speeds
+  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+    byte i2cSend[4];
+    i2cSend[0] = (rightSpeed) & 0xFF;
+    i2cSend[1] = (rightSpeed >> 8) & 0xFF;
+    i2cSend[2] = (leftSpeed) & 0xFF;
+    i2cSend[3] = (leftSpeed >> 8) & 0xFF;
+
+    Wire.beginTransmission(N1_I2C_ADDR);
+    Wire.write(i2cSend,4);
+    Wire.endTransmission(N1_I2C_ADDR);
+  }
+
+
+#else
+  #error A motor driver must be selected!
+#endif
+
+#endif
