311fd4506ad0d54f9908b4d163458de7a3eeaab7
Vinaykumar Hegde <me@vnay.in>
2016-05-02 13:53:28 -0700

    add i2c request, change baudrate to 115200

--- hcrbot_gmr_n2.ino
+++ hcrbot_gmr_n2.ino
@@ -12,10 +12,13 @@
  *      UR_L2SE - 0x16
  * 
  */
-
-#undef DEBUG
  
 #include "URM04Driver.h"
+#include <Wire.h>
+#define N2_I2C_ADDR 0x11
+#define BAUDRATE 115200
+
+#define DEBUG 1
 
 
 const uint8_t numSensors = 6; // Number of sensors
@@ -23,11 +26,20 @@ const byte initAdd = 0x11; // Initial address of sensor bunch.
 URM04Sensor l[numSensors]; // URM04 sensors.
 byte distances[2*numSensors]; // 2 bytes for each sensor distance data.
 
+/* Send the URM readings through I2C */
+void sendURMReadings(){
+#ifdef DEBUG
+  Serial.println("Sending URM Readings...");
+#endif
+  Wire.write(distances,2*numSensors);
+}
+
 void setup(){
-  Serial.begin(19200);
+  Serial.begin(BAUDRATE);
   Serial.println("Initalizing....");
-//  setAdd(0x11);
   initURM(l,numSensors,initAdd);
+  Wire.begin(N2_I2C_ADDR);
+  Wire.onRequest(sendURMReadings);
 }
 
 void readURMDist(URM04Sensor l[], uint8_t num, byte dist[]){
@@ -39,22 +51,16 @@ void readURMDist(URM04Sensor l[], uint8_t num, byte dist[]){
     uint16_t di = l[i].ReadDistance();
     dist[i] = di/256; 
     dist[i+1] = di % 256; 
+#ifdef DEBUG
     Serial.print("Distance (sensor:");
     Serial.print(i); Serial.print(": ");
     Serial.println(di);
+#endif
     clearRxBuffer();
   }
 }
 
 void loop(){
-//  l[0].Trigger();
-//  delay(30);// 30ms delay after trigger
-//  l[0].ReadSignal();
-//  delay(10); // 10ms delay after read signal
-//  uint16_t dist = l[0].ReadDistance();
-//  Serial.print("Distance: ");
-//  Serial.println(dist);
-//  clearRxBuffer();
   readURMDist(l,numSensors,distances);
   delay(500);
 }
